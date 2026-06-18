f46c6781eae7f3967fe870ea3db1a28b45f393c6
yvesconst <yvesconst@yahoo.fr>
2026-06-18 14:49:49 +0200

    Fix yaml file

--- vars/formatParamsToYaml.groovy
+++ vars/formatParamsToYaml.groovy
@@ -1,11 +1,13 @@
 def call(Map paramsMap, String targetFilePath = 'vars_feeder.yml') {
     echo "[CI] Formatage des paramètres en YAML..."
 
-    def builder = new YamlBuilder()
-    builder(paramsMap)
+//     def builder = new YamlBuilder()
+//     builder(paramsMap)
+
+    writeYaml file: targetFilePath, data: paramsMap
 
     // Écriture du fichier YAML dans le workspace de l'agent
-    writeFile file: targetFilePath, text: builder.toString()
+//     writeFile file: targetFilePath, text: builder.toString()
     echo "[CI] Fichier YAML généré avec succès : ${targetFilePath}"
 
     return targetFilePath
